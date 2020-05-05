5e1da5ec59cf2f1517ba73d20538975913fb6b59
Danielle <daniellerodris@gmail.com>
2020-05-04 22:15:05 -0300

    Salvando e atualizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main(){
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  Firestore.instance
-    .collection("usuarios")
-    .document("pontuacao")
-    .setData({"carlos": "80", "silvana": "340"});
+
+  Firestore db = Firestore.instance;
+
+  /*
+  db.collection("usuarios")
+    .document("002")
+    .setData({
+      "nome": "Ana Maria",
+      "idade": "25"
+    });*/
+
+  /*
+  DocumentReference ref = await db.collection("noticias")
+  .add({
+    "titulo": "Ondas de calor em São Paulo",
+    "descricao": "texto de exemplo..."
+  });*/
+
+  //print("item salvo: " + ref.documentID);
+
+  db.collection("noticias")
+    .document("2HR83NA7Si5hUEubrbUf")
+    .setData({
+      "titulo": "Ondas de calor em São Paulo alterado",
+      "descricao": "texto de exemplo..."
+    });
 
   runApp(App());
 }
